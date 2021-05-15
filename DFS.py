3f5cc0ff9fa580bb236ad9a3965c1cf857505b54
KemerDev <kemerios10@gmail.com>
2021-05-16 00:26:23 +0300

    Initial Commit

--- DFS.py
+++ DFS.py
@@ -7,8 +7,6 @@ from MazeCreate import create_maze as m
 def DFS_algo():
     maze = m()
     maze_cords = []
-    maze_walls = []
-    num_wall = []
     notvisited = []
     start = []
     finish = []
@@ -33,16 +31,35 @@ def DFS_algo():
         for j, h in enumerate(c):
             if h == ' ' or h == 'G' or h == 'S':
                 notvisited.append([i, j])
-            else:
-                maze_walls.append([i, j])
-
-    for i, c in enumerate(maze_walls):
-        num_wall.append(i)
 
     stack.append(start)
 
     for i, c in enumerate(notvisited):
         if stack in c:
             notvisited.pop(i)
+
+    while stack:
+        step = stack.pop()
+        num = []
+
+        if step[0] == finish[0]:
+            for i in maze:
+                print(i)
+            break
+
+        for j, h in enumerate(notvisited):
+            if h == step:
+                notvisited.pop(j)
+                break
+            for k in j:
+                for i in maze_cords[k]:
+                    if i in notvisited:
+                        stack.append([i])
+                        if maze[i[0]][i[1]] != 'S' and maze[i[0]][i[1]] != 'S' and maze[i[0]][i[1]] != 'G':
+                            maze[i[0]][i[1]] = 'W'
+
+            time.sleep(1)
+            for i in maze:
+                print(i, end='\n', flush=True)
 
 DFS_algo()
